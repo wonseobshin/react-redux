fc47eb63e3bca1d4b8d1330019d0a9be62cc74cc
Won Seob Shin <wonseob07@gmail.com>
2020-11-20 21:56:29 +0000

    init

---
+++ src/components/ExampleComponent.jsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+class ExamplaeComponent extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            articles: [
+                { title: "title 1", id:1 },
+                { title: "title 2", id:2 }
+            ]
+        };
+    }
+    render() {
+        const {articles}= this.state;
+    return <>
+    <ul> {articles.map(el => <li key={el.id}>{el.title}</li>)} </ul>
+    </>
+    }
+}
